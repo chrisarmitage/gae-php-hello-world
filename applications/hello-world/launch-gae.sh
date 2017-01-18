0b18600674445513ab5cab4cafc6d1d03a077e6c
Chris Armitage <dev@chrisarmitage.com>
2017-01-18 12:20:14 +0000

    Formatting on the launch-gae.sh, causing issues with line endings

--- applications/hello-world/launch-gae.sh
+++ applications/hello-world/launch-gae.sh
@@ -1,4 +1,1 @@
-dev_appserver.py app.yaml\
-  --host=0.0.0.0\
-  --admin_host=0.0.0.0\
-  --php_executable_path=/usr/bin/php-cgi
+dev_appserver.py app.yaml --host=0.0.0.0 --admin_host=0.0.0.0 --php_executable_path=/usr/bin/php-cgi
